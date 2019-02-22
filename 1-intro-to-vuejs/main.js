eae2c01ae390da66ce3d1affa55424c8b8228918
= <chase.terry87@gmail.com>
2019-02-22 13:20:52 -0600

    Completed lesson 4 + challenge.

--- 1-intro-to-vuejs/main.js
+++ 1-intro-to-vuejs/main.js
@@ -6,5 +6,34 @@ var app = new Vue({
         image: './assets/greensocks.jpeg',
         inventory: 100,
         onSale: false,
+        details: [
+            "80% cotton",
+            "20% polyester",
+            "Gender-neutral"
+        ],
+        variants: [
+            {
+                variantId: 1234,
+                variantColor: "green"
+            },
+            {
+                variantId: 5678,
+                variantColor: "blue"
+            }
+        ],
+        sizes: [
+            {
+                sizeId: 123,
+                sizeDescription: "Small",
+            },
+            {
+                sizeId: 456,
+                sizeDescription: "Medium",
+            },
+            {
+                sizeId: 789,
+                sizeDescription: "Large",
+            }
+        ]
     }
 });
