4b36aeee8f9c3fc5b2cdad748a990e57db66e144
= <chase.terry87@gmail.com>
2019-02-22 15:33:49 -0600

    Completed lesson 7

--- 1-intro-to-vuejs/main.js
+++ 1-intro-to-vuejs/main.js
@@ -1,11 +1,10 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: 'VueMastery',
         product: 'Socks',
         description: 'These are the most comfortable socks!',
-        image: './assets/greensocks.jpeg',
-        inventory: 100,
-        inStock: false,
+        selectedVariant: 0,
         onSale: false,
         details: [
             "80% cotton",
@@ -16,12 +15,14 @@ var app = new Vue({
             {
                 variantId: 1234,
                 variantColor: "green",
-                variantImage: './assets/greensocks.jpeg'
+                variantImage: './assets/greensocks.jpeg',
+                variantQuantity: 10
             },
             {
                 variantId: 5678,
                 variantColor: "blue",
-                variantImage: './assets/bluesocks.jpeg'
+                variantImage: './assets/bluesocks.jpeg',
+                variantQuantity: 0
             }
         ],
         sizes: [
@@ -49,8 +50,19 @@ var app = new Vue({
                 this.cart--;
             }
         },
-        updateProduct: function (variantImage) {
-            this.image = variantImage
+        updateProduct: function(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
         }
     }
 });
